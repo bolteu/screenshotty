6c847ff2ef8a0d6537b6599d7cdda8f3ae0ef705
yarolegovich <yarolegovich@gmail.com>
2019-10-26 22:26:47 +0300

    re #3: removed Falcon from project
    The decision was made because:
    * Falcon draws all views it finds in WindowManager, some of them might belong to stopped activities.
    * Because of the previous point, dialogs might be handled incorrectly.
    * If reflection fails - nothing will be drawn, because it's used even to access decor view of the activity, even though it's available via public API.
    * Our default fallback strategy now renders dialogs and popup windows :yay:

--- screenshotty-lib/src/main/java/eu/bolt/screenshotty/falcon/FalconFallbackStrategy.kt
+++
@@ -1,9 +0,0 @@
-package eu.bolt.screenshotty.falcon
-
-import android.app.Activity
-import android.graphics.Bitmap
-import eu.bolt.screenshotty.FallbackStrategy
-
-class FalconFallbackStrategy : FallbackStrategy {
-    override fun takeScreenshot(activity: Activity): Bitmap = Falcon.takeScreenshotBitmap(activity)
-}
