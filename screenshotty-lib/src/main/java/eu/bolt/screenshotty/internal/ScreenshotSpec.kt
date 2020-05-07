281306e058ff7c331bc987ae07b4023be98ba4fb
xzaleksey <aleksei.valiakin@bolt.eu>
2020-05-07 11:38:09 +0300

    Added fix for getting size of display for ScreenshotSpec.kt.

--- screenshotty-lib/src/main/java/eu/bolt/screenshotty/internal/ScreenshotSpec.kt
+++ screenshotty-lib/src/main/java/eu/bolt/screenshotty/internal/ScreenshotSpec.kt
@@ -2,6 +2,8 @@ package eu.bolt.screenshotty.internal
 
 import android.app.Activity
 import android.graphics.Point
+import android.os.Build
+import android.view.Display
 
 internal class ScreenshotSpec constructor(activity: Activity) {
 
@@ -11,9 +13,19 @@ internal class ScreenshotSpec constructor(activity: Activity) {
 
     init {
         val display = activity.windowManager.defaultDisplay
-        val displaySize = Point().apply(display::getSize)
+        val displaySize = getDisplaySize(display)
         width = displaySize.x
         height = displaySize.y
         densityDpi = activity.resources.displayMetrics.densityDpi
+    }
+
+    private fun getDisplaySize(display: Display): Point {
+        return Point().apply {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                display.getRealSize(this)
+            } else {
+                display.getSize(this)
+            }
+        }
     }
 }
