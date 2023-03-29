59cd5d5d57d198782a5b1687017b6ea6fc2ec5d3
Oleg Sokolov <sokolov.ob@gmail.com>
2023-03-29 14:51:35 +0300

    Update screenShotManagerExt.kt
    make "ScreenshotManager.makeScreenshotAsync()" return a screenshot

--- screenshotty-coroutines-lib/src/main/java/eu/bolt/screenshotty/coroutines/screenShotManagerExt.kt
+++ screenshotty-coroutines-lib/src/main/java/eu/bolt/screenshotty/coroutines/screenShotManagerExt.kt
@@ -1,16 +1,14 @@
 package eu.bolt.screenshotty.coroutines
 
-import eu.bolt.screenshotty.Screenshot
 import eu.bolt.screenshotty.ScreenshotManager
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
-suspend fun ScreenshotManager.makeScreenshotAsync() {
-    suspendCoroutine<Screenshot> {
+suspend fun ScreenshotManager.makeScreenshotAsync() =
+    suspendCoroutine {
         makeScreenshot().observe(
             onSuccess = it::resume,
             onError = it::resumeWithException
         )
     }
-}
